eb5fda36dc6268e19d86549dde869d6b7eceac77
Tory Kulick <30578137+tkulick@users.noreply.github.com>
2018-07-11 13:05:16 -0400

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -36,8 +36,6 @@ exp_continue}
 }
 EOT
 
-chown -R spigot:spigot /home/spigot
-
 # Setup a Bash script for the install
 cat <<EOF >> /home/spigot/McMyAdmin/setup.sh
 #!/bin/bash
@@ -50,17 +48,19 @@ chmod +x /home/spigot/McMyAdmin/install.sh
 tmux new -s McMyAdmin -d /home/spigot/McMyAdmin/MCMA2_Linux_x86_64
 EOF
 
+chown -R spigot:spigot /home/spigot
+
 su - spigot -c "chmod +x /home/spigot/McMyAdmin/setup.sh && /home/spigot/McMyAdmin/setup.sh"
 
 # Install and compile!
-su - spigot -c "mkdir /home/spigot/BuildTools && mkdir /home/spigot/spigot"
-su - spigot -c "wget -O ~/BuildTools/BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
-su - spigot -c "git config --global --unset core.autocrlf"
-su - spigot -c "java -jar ~/BuildTools/BuildTools.jar"
+#su - spigot -c "mkdir /home/spigot/BuildTools && mkdir /home/spigot/spigot"
+#su - spigot -c "wget -O ~/BuildTools/BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
+#su - spigot -c "git config --global --unset core.autocrlf"
+#su - spigot -c "java -jar ~/BuildTools/BuildTools.jar"
 
 # Mark EULA as true and fire up the server
-su - spigot -c "java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar ~/spigot*.jar"
-su - spigot -c "sed -i \"s/eula=false/eula=true/\" /home/spigot/eula.txt"
+#su - spigot -c "java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar ~/spigot*.jar"
+#su - spigot -c "sed -i \"s/eula=false/eula=true/\" /home/spigot/eula.txt"
 
 # Commenting out Spigot launch since McMyAdmin will launch the server
 #su - spigot -c "tmux new -s spigot -d 'java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar /home/spigot/spigot*.jar'"
