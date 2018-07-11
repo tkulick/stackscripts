a4fddcb716796721151b2fe245adff542881975e
Tory Kulick <30578137+tkulick@users.noreply.github.com>
2018-07-11 12:24:01 -0400

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -18,19 +18,6 @@ apt -q -y install git openjdk-8-jre-headless expect unzip
 adduser --disabled-password --gecos "" spigot
 chown -R spigot:spigot /home/spigot
 
-# Install and compile!
-su - spigot -c "mkdir /home/spigot/BuildTools && mkdir /home/spigot/spigot"
-su - spigot -c "wget -O ~/BuildTools/BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
-su - spigot -c "git config --global --unset core.autocrlf"
-su - spigot -c "java -jar ~/BuildTools/BuildTools.jar"
-
-# Mark EULA as true and fire up the server
-su - spigot -c "java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar ~/spigot*.jar"
-su - spigot -c "sed -i \"s/eula=false/eula=true/\" /home/spigot/eula.txt"
-
-# Commenting out Spigot launch since McMyAdmin will launch the server
-#su - spigot -c "tmux new -s spigot -d 'java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar /home/spigot/spigot*.jar'"
-
 # Install McMyAdmin
 cd /usr/local
 wget http://mcmyadmin.com/Downloads/etc.zip
@@ -56,4 +43,17 @@ chown -R spigot:spigot /home/spigot
 
 su - spigot -c "mv /home/spigot/MCMA2_Linux_x86_64 /home/spigot/McMyAdmin/"
 su - spigot -c "chmod +x /home/spigot/McMyAdmin/install.sh"
-su - spigot -c "/home/spigot/McMyAdmin/install.sh && tmux new -s McMyAdmin -d /home/spigot/McMyAdmin/MCMA2_Linux_x86_64"
+su - spigot -c "cd /home/spigot/McMyAdmin && /home/spigot/McMyAdmin/install.sh && tmux new -s McMyAdmin -d /home/spigot/McMyAdmin/MCMA2_Linux_x86_64"
+
+# Install and compile!
+su - spigot -c "mkdir /home/spigot/BuildTools && mkdir /home/spigot/spigot"
+su - spigot -c "wget -O ~/BuildTools/BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
+su - spigot -c "git config --global --unset core.autocrlf"
+su - spigot -c "java -jar ~/BuildTools/BuildTools.jar"
+
+# Mark EULA as true and fire up the server
+su - spigot -c "java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar ~/spigot*.jar"
+su - spigot -c "sed -i \"s/eula=false/eula=true/\" /home/spigot/eula.txt"
+
+# Commenting out Spigot launch since McMyAdmin will launch the server
+#su - spigot -c "tmux new -s spigot -d 'java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar /home/spigot/spigot*.jar'"
