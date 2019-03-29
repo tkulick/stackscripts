7e893c4030ab1d89839edb8a0058b5d891c6b738
Tory Kulick <30578137+tkulick@users.noreply.github.com>
2019-03-29 16:14:09 -0400

    Update plex.sh

--- plex.sh
+++ plex.sh
@@ -6,15 +6,14 @@
 #<UDF name="fqdn" label="Fully Qualified Domain Name" example="Provide the domain name you'd like to use for your server">
 # FQDN=
 #
-#<udf name="gameserver" label="Game Server" oneOf="arkserver,arma3server,bb2server,bdserver,bf1942server,bmdmserver,boserver,bsserver,bt1944server,ccserver,cod2server,cod4server,codserver,coduoserver,codwawserver,csczserver,csgoserver,csserver,cssserver,dabserver,dmcserver,dodserver,dodsserver,doiserver,dstserver,emserver,etlserver,fctrserver,fofserver,gesserver,gmodserver,hl2dmserver,hldmserver,hldmsserver,hwserver,insserver,jc2server,jc3server,kf2server,kfserver,l4d2server,l4dserver,mcserver,mtaserver,mumbleserver,nmrihserver,ns2cserver,ns2server,opforserver,pcserver,pvkiiserver,pzserver,q2server,q3server,qlserver,qwserver,ricochetserver,roserver,rustserver,rwserver,sampserver,sbserver,sdtdserver,squadserver,ss3server,stserver,svenserver,terrariaserver,tf2server,tfcserver,ts3server,tuserver,twserver,ut2k4server,ut3server,ut99server,wetserver,zpsserver" example="Select your game for your game server">
-# GAMESERVER=
-#
-#<UDF name="gamename" label="Game Server Name" example="Name of the game server within your game">
-# GAMENAME=
-#
 # Version control: https://github.com/tkulick/stackscripts
 #
 
+# Update packages
+apt-get -y update && apt-get -y dist-upgrade && apt-get -y autoremove
 
-
+# Grab the Plex installation file
 wget -O plex.deb https://downloads.plex.tv/plex-media-server-new/1.15.3.835-5d4a5c895/debian/plexmediaserver_1.15.3.835-5d4a5c895_amd64.deb
+
+# Install Plex
+dpkg -i plex.deb
