f155db35ef503d06a70992e5cb2026c7e245a7c3
Tory Kulick <30578137+tkulick@users.noreply.github.com>
2019-03-29 17:31:11 -0400

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #
+#
+#<UDF name="pubkey" label="SSH Public Key">
+# PUBKEY=
+#
 #<UDF name="cluster" label="Cluster Name" example="mycluster">
 # CLUSTER=
 #
@@ -21,10 +25,14 @@
 # Version control: https://github.com/tkulick/stackscripts
 #
 
+# Add SSH directory and update pub key
+mkdir .ssh
+echo $PUBKEY > .ssh/id_rsa.pub
+
 # Download and install pip and the Linode CLI
 apk add py-pip
 pip install linode-cli
 
-#
-linode-cli k8s-alpha create $CLUSTER --node-type $NODETYPE --nodes $NODECOUNT --master-type $MASTERTYPE --region $REGION --ssh-public-key $TOKEN
+# Install cluster
+linode-cli k8s-alpha create $CLUSTER --node-type $NODETYPE --nodes $NODECOUNT --master-type $MASTERTYPE --region $REGION --ssh-public-key $HOME/.ssh/id_rsa.pub options --api-key $TOKEN
 
